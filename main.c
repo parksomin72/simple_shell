9b5a7574cc011ec57b9c132c32129b7ac44d015e
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-08 21:14:52 -0800

    shell

--- main.c
+++ main.c
@@ -1,6 +1,58 @@
-#include "shell.h"
-int main(void) {
-    char *args[] = {"ls", "test1", "test2", NULL};
-    execute_command(args);
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+int main()
+{
+    char command[MAX_COMMAND_LENGTH];
+    pid_t pid;
+
+    while (1)
+    {
+        printf("#cisfun$ ");
+        fflush(stdout);
+
+        /* Read command from user */
+        if (fgets(command, sizeof(command), stdin) == NULL)
+	{
+            /* Handling Ctrl+D (end of file) */
+            printf("\n");
+            break;
+        }
+
+        /* Remove trailing newline character */
+        command[strcspn(command, "\n")] = 0;
+
+        /* Fork a child process */
+        pid = fork();
+
+        if (pid == -1)
+	{
+            /* Fork failed */
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+	else if (pid == 0)
+	{
+            /* Child process */
+            /* Execute the command */
+            if (execlp(command, command, (char *)NULL) == -1) {
+                /* If exec fails, print error */
+                printf("%s: command not found\n", command);
+                exit(EXIT_FAILURE);
+            }
+        }
+	else
+	{
+            /* Parent process */
+            /* Wait for child to terminate */
+            wait(NULL);
+        }
+    }
+
+    return (0);
 }
--- memory.c
+++
@@ -1,8 +0,0 @@
-#include "shell.h"
-/**
- * Function to free allocated memory for arguments
- */
-void free_args(char **args)
-{
-    free(args);
-}
