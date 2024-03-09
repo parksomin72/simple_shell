f06413765e5bfddcc05d1c09919f7a098b39303f
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-08 21:45:35 -0800

    shell

--- main.c
+++ main.c
@@ -5,40 +5,54 @@
 
 #define BUFFER_SIZE 1024
 
-int main(void) {
+/**
+ * main - Simple shell program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
     char buffer[BUFFER_SIZE];
     ssize_t read_bytes;
     pid_t child_pid;
     int status;
     char *envp[] = {NULL};
 
-    while (1) {
+    while (1)
+    {
         printf("#cisfun$ ");
         read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-        if (read_bytes == -1) {
+        if (read_bytes == -1)
+        {
             perror("read");
             exit(EXIT_FAILURE);
         }
-        if (read_bytes == 0) {
+        if (read_bytes == 0)
+        {
             printf("\n");
             break;
         }
         buffer[read_bytes - 1] = '\0';
-        
+
         child_pid = fork();
-        if (child_pid == -1) {
+        if (child_pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        if (child_pid == 0) {
+        if (child_pid == 0)
+        {
             char *args[2];
             args[0] = buffer;
             args[1] = NULL;
-            if (execve(buffer, args, envp) == -1) {
+            if (execve(buffer, args, envp) == -1)
+            {
                 perror("execve");
                 exit(EXIT_FAILURE);
             }
-        } else {
+        }
+        else
+        {
             wait(&status);
         }
     }
