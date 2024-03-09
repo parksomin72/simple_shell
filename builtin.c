130cabc8382b32a15237189473495eb408dd372c
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-09 00:07:47 -0800

    .

--- builtin.c
+++ builtin.c
@@ -1,29 +1,27 @@
 #include "shell.h"
+
 /**
- * execute_builtin_command - This function takes a string representing
- *                           the command to execute.
+ * execute_builtin_command - Execute a built-in command
  * @command: The command to execute
- *
  */
 void execute_builtin_command(char *command)
 {
-	char **env;
-
-	/* Code to execute built-in commands*/
-	if (strcmp(command, "exit") == 0)
-	{
-		exit(0); /* Exit the shell*/
-	}
-	else if (strcmp(command, "env") == 0)
-	{
-		/* Print the environment var*/
-		for (env = environ; *env != NULL; env++)
-		{
-			printf("%s\n", *env);
-		}
-	}
-	else
-	{
-		printf("Unknown command: %s\n", command);
-	}
+    if (strcmp(command, "exit") == 0)
+    {
+        /* Exit the shell */
+        exit(EXIT_SUCCESS);
+    }
+    else if (strcmp(command, "env") == 0)
+    {
+        /* Print the environment variables */
+        char **env;
+        for (env = environ; *env != NULL; env++)
+        {
+            printf("%s\n", *env);
+        }
+    }
+    else
+    {
+        printf("Unknown command: %s\n", command);
+    }
 }
