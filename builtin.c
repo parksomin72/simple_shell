e1ba03f0f739f235d5d961d91b473f473d857e65
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-09 00:10:43 -0800

    .

--- builtin.c
+++ builtin.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <stdlib.h>
 
 /**
  * execute_builtin_command - Execute a built-in command
@@ -6,10 +7,21 @@
  */
 void execute_builtin_command(char *command)
 {
+    char *token;
+    int status;
+
     if (strcmp(command, "exit") == 0)
     {
-        /* Exit the shell */
+        /* Exit the shell with default status 0 if no argument provided */
         exit(EXIT_SUCCESS);
+    }
+    else if (strncmp(command, "exit ", 5) == 0)
+    {
+        /* Parse the argument to get the exit status */
+        token = strtok(command, " ");
+        token = strtok(NULL, " ");
+        status = atoi(token);
+        exit(status);
     }
     else if (strcmp(command, "env") == 0)
     {
