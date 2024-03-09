af28ee6762bc1554d7dc846a275e36687ca0b7f1
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-09 00:33:46 -0800

    .

--- exec.c
+++ exec.c
@@ -1,18 +1,29 @@
 #include "shell.h"
+/**
+ * execute_command - Execute a command with the given arguments
+ * @args: Array of strings containing the command and its arguments
+ */
+void execute_command(char **args)
+{
+	pid_t pid = fork();
 
-void execute_command(char **args) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        /* Child process */
-        if (execvp(args[0], args) == -1) {
-            perror(args[0]);
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        /* Parent process */
-        wait(NULL);
-    }
+	if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		/* Child process */
+		if (execvp(args[0], args) == -1)
+		{
+			perror(args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* Parent process */
+		wait(NULL);
+	}
 }
--- main.c
+++ main.c
@@ -3,35 +3,40 @@
  * main - Entry point
  * Return: alwys 0 (Seccuss)
  */
-int main(void) {
-    char input[MAX_INPUT_LENGTH];
-    char *args[MAX_ARGS];
-    int status;
-    char *token;
-    int i;
+int main(void)
+{
+	char input[MAX_INPUT_LENGTH];
+	char *args[MAX_ARGS];
+	int status;
+	char *token;
+	int i;
 
-    while (1) {
-        printf("($) ");
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            perror("fgets");
-            exit(EXIT_FAILURE);
-        }
+	while (1)
+	{
+		printf("($) ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			perror("fgets");
+			exit(EXIT_FAILURE);
+		}
 
-        /* Tokenize the input */
-        token = strtok(input, DELIMITER);
-        i = 0;
-        while (token != NULL && i < MAX_ARGS - 1) {
-            args[i++] = token;
-            token = strtok(NULL, DELIMITER);
-        }
-        args[i] = NULL;
+		/* Tokenize the input */
+		token = strtok(input, DELIMITER);
+		i = 0;
 
-        /* Execute the command */
-        execute_command(args);
+		while (token != NULL && i < MAX_ARGS - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, DELIMITER);
+		}
+		args[i] = NULL;
 
-        /* Wait for child process to terminate*/
-        wait(&status);
-    }
+		/* Execute the command */
+		execute_command(args);
 
-    return (0);
+		/* Wait for child process to terminate*/
+		wait(&status);
+	}
+
+	return (0);
 }
